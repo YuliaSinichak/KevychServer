4bdace121cbcee8cc151215114f1945a6127b14a
YuliaSinichak <yuliasinichak@gmail.com>
2024-09-24 00:25:54 +0300

    trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use((req, res, next) => {
+    req.app.set('trust proxy', 1);
+  });
+
   app.enableCors({
     origin: 'https://kevych-client.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
