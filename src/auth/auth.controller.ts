06adcf0fdb8fafcc2a5fb7de11eee26005aa9320
YuliaSinichak <yuliasinichak@gmail.com>
2024-09-24 16:41:25 +0300

    jwt res maxAge

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -65,6 +65,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'none',
+      maxAge: 24 * 60 * 60 * 1000,
     });
 
     return {
