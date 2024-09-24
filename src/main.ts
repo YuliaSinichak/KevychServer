8d95d7f5d0e3b72bc6e6ed331ec584b8fba96421
YuliaSinichak <yuliasinichak@gmail.com>
2024-09-24 15:30:23 +0300

    make rsc *

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.set('trust proxy', 1);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
